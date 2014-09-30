246b46a2d025ed07d1ff28f80c4f7c2b02f8321d
Sebastien Hutt <seb.hutt@gmail.com>
2014-09-30 16:09:01 -0700

    added enemy animation and shoot ability

--- examples/simple_cube/simple_cube.js
+++ examples/simple_cube/simple_cube.js
@@ -2,13 +2,19 @@ function main() {
 	var myScene = new LIME.Scene('myCanvas');
 	var gl = myScene.getContext();
 
-	var cubeGeometry = new LIME.Geometry();
-	cubeGeometry.createCube(0.5,0.5,5.0);
+	var cubeGeometry = new LIME.Geometry(myScene);
+	//cubeGeometry.createCube(0.5,0.5,5.0);
+	//cubeGeometry.createRectangle(0.5, 0.5, 0.0);
+	cubeGeometry.createCircle(0.2, 6);
 
 	var cubeMaterial = new LIME.FlatShader(cubeGeometry, gl, 1.0, 0.0, 0.0, 1.0);
 
-	var cube = new LIME.Shape(cubeGeometry, cubeMaterial, gl, LIME.drawTriangleFan);
+	var cube = new LIME.Shape(cubeGeometry, cubeMaterial, gl, LIME.drwaPoints);
 	cube.setPosition(0.0, 0.0, 0.0);
+
+	var ratio = myScene.getAspectRatio();
+	console.log(ratio[0]);
+	console.log(ratio[1]);
 
 	myScene.setClearColor(0.9, 0.9, 0.9, 1.0);
 	myScene.clearCanvas();
