b38ec2184487a23acad786c96536dadb1c76c0c2
Sigf <seb.hutt@gmail.com>
2014-12-07 12:50:26 -0800

    Removes concatenation from shader

--- src/material/flatMaterial.js
+++ src/material/flatMaterial.js
@@ -2,30 +2,29 @@
 // - Add uniform for a_Color instead of concatenate
 // - Add uniform for ambient light
 // - Add uniform declaration in class
+// - Add controls to change uiniforms
 
 LIME.FlatMaterial = function(gl, r, g, b, a, lighting) {
 
   this.context = gl;
   this.program;
-  this.red = r;
-  this.green = g;
-  this.blue = b;
-  this.alpha = a;
+  this.color = [r, g, b, a];
   this.type = LIME.flatMaterial;
   this.isLit = lighting;
   
   if(this.isLit) {
     var VSHADER_SOURCE = 
     'attribute vec4 a_Position;\n' +
+    'attribute vec4 a_Normal;\n' +
+    'uniform vec4 u_Color;\n' +
     'uniform mat4 u_MvpMatrix;\n' +
     'uniform mat4 u_NormalMatrix;\n' +
-    'attribute vec4 a_Normal;\n' +
     'uniform vec3 u_LightColor;\n' +
     'uniform vec3 u_LightDirection;\n' +
 
     'varying vec4 v_Color;\n' +
     'void main() {\n' +
-    '  vec4 a_Color = vec4(' + (r).toFixed(1) + ', ' + (g).toFixed(1) + ', ' + (b).toFixed(1) + ', ' + (a).toFixed(1) + ');\n' +
+    '  vec4 a_Color = vec4( u_Color );\n' +
     '  vec3 ambient = vec3(0.2, 0.2, 0.2) * a_Color.rgb;\n' +
     '  gl_Position = u_MvpMatrix * a_Position;\n' +
     '  vec3 normal = normalize(vec3(u_NormalMatrix * a_Normal));\n' +
@@ -47,25 +46,40 @@ LIME.FlatMaterial = function(gl, r, g, b, a, lighting) {
     var VSHADER_SOURCE = 
     'attribute vec4 a_Position;\n' +
     'uniform mat4 u_MvpMatrix;\n' +
+    'uniform vec4 u_Color;\n' +
+    'varying vec4 v_Color;\n' +
     'void main() {\n' +
     '  gl_Position = u_MvpMatrix * a_Position;\n' +
     '  gl_PointSize = 5.0;\n' +
+    '  v_Color = u_Color;\n' +
     '}\n' ;
 
     var FSHADER_SOURCE =
     '#ifdef GL_ES\n' +
     'precision mediump float;\n' +
     '#endif GL_ES\n' +
+    'varying vec4 v_Color;\n' +
     'void main() {\n' + 
-    '  gl_FragColor = vec4(' + (r).toFixed(1) + ', ' + (g).toFixed(1) + ', ' + (b).toFixed(1) + ', ' + (a).toFixed(1) + ');\n' +
+    '  gl_FragColor = v_Color);\n' +
     '}\n';
     }
+
   var program = createProgram(this.context, VSHADER_SOURCE, FSHADER_SOURCE);
   if (!program) {
     console.log('Failed to create program');
     return false; 
   }
   this.program = program;
+
+  this.a_Position = createAttribLocation(gl, this.program, 'a_Position');
+  this.a_Normal = createAttribLocation(gl, this.program, 'a_Normal');
+
+  this.u_Color = createUniformLocation(gl, this.program, 'u_Color');
+  this.u_MvpMatrix = createUniformLocation(gl, this.program, 'u_Color');
+  this.u_NormalMatrix = createUniformLocation(gl, this.program, 'u_Color');
+  this.u_LightColor = createUniformLocation(gl, this.program, 'u_Color');
+  this.u_LightDirection = createUniformLocation(gl, this.program, 'u_Color');
+
 };
 
 LIME.FlatMaterial.prototype.getProgram = function() {
@@ -74,5 +88,13 @@ LIME.FlatMaterial.prototype.getProgram = function() {
 
 LIME.FlatMaterial.prototype.getType = function() {
   return this.type;
+};
+
+LIME.FlatMaterial.prototype.getColor = function() {
+  return new Vector4(this.color);
+};
+
+LIME.FlatMaterial.prototype.setColor = function(r, g, b, a) {
+  this.color = [r, g, b, a];
 };
 
