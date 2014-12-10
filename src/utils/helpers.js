1c67b9454539855811894ca8d9771278bcc24033
Sigf <seb.hutt@gmail.com>
2014-12-09 18:28:35 -0800

    Adds helpers function to converts hex value to vector

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16,3 +16,37 @@ createUniformLocation = function(gl, program, name) {
 	return attrib_pointer;
 }
 
+hexToColorArray = function(str) {
+	var r = str.substring(1,4);
+	var g = str.substring(3,5);
+	var b = str.substring(5,7);
+
+	r = parseInt(r, 16);
+	g = parseInt(g, 16);
+	b = parseInt(b, 16);
+
+	r /= 255;
+	g /= 255;
+	b /= 255;
+
+	var arr = [r, g, b];
+	return arr;
+}
+
+hexToColorVector = function(str) {
+	var r = str.substring(1,3);
+	var g = str.substring(3,5);
+	var b = str.substring(5,7);
+
+	r = parseInt(r, 16);
+	g = parseInt(g, 16);
+	b = parseInt(b, 16);
+
+	r /= 255;
+	g /= 255;
+	b /= 255;
+
+	var arr = [r, g, b];
+	return new Vector3(arr);
+}
+
